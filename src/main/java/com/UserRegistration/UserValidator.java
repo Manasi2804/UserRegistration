2ebf8e3438f55d44604fa48c54895efaf0ce2a9f
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-02 12:14:06 +0530

    Added: To validate first name

--- src/main/java/com/UserRegistration/UserValidator.java
+++ src/main/java/com/UserRegistration/UserValidator.java
@@ -1,7 +1,17 @@
 package com.UserRegistration;
 
-public class UserValidator
-{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserValidator {
+
+    public boolean validateFirstName(String firstName) {
+        String patternForFirstName = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(patternForFirstName);
+        Matcher matcher = p.matcher(firstName);
+        return matcher.matches();
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to user registration");
     }

029dd7822c7b5778b65f2a1fc1050c25389aa2a2
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-02 12:27:50 +0530

    Added: To validate last name

--- src/main/java/com/UserRegistration/UserValidator.java
+++ src/main/java/com/UserRegistration/UserValidator.java
@@ -4,14 +4,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-
-    public boolean validateFirstName(String firstName) {
+    // Method for validating first name using Regex
+    public boolean validateFirstName(String firstName)
+    {
         String patternForFirstName = "^[A-Z]{1}[a-z]{2,}$";
         Pattern p = Pattern.compile(patternForFirstName);
         Matcher matcher = p.matcher(firstName);
         return matcher.matches();
     }
 
+    // Method for validating last name using Regex
+    public boolean validateLastName(String lastName)
+    {
+        String patternForLastName="^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(patternForLastName);
+        Matcher matcher = p.matcher(lastName);
+        return matcher.matches();
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to user registration");
     }
